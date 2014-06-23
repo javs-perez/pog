5d17415a7e93d2ff4609b90d1dcface9c728ac98
Javier Perez <perez.javi.a@gmail.com>
2014-06-22 22:16:36 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PogApp::Application.config.secret_key_base = 'e402c95faa1cdf0266d43ce5f29b48543a09877fce7fcd5efe93f5e4eb0401f44f568291980e415e5d42575c0feaf8ff5c843931a8b996be94dc1792f4506847'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PogApp::Application.config.session_store :cookie_store, key: '_pog_app_session'
